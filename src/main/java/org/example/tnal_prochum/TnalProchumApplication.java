5f6137e434f63309de8e5c4183fd1c46bbc5227a
Phaeng Sopheavorleak <phaengsopheavorleak@gamil.com>
2026-06-03 08:54:56 +0700

    fix timezone Cambodia

--- src/main/java/org/example/tnal_prochum/TnalProchumApplication.java
+++ src/main/java/org/example/tnal_prochum/TnalProchumApplication.java
@@ -3,10 +3,19 @@ package org.example.tnal_prochum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class TnalProchumApplication {
+
+    @PostConstruct
+    public void init() {
+        // Set timezone to Cambodia
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Phnom_Penh"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TnalProchumApplication.class, args);
     }
